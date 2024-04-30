69307df331bacc87f387ca2053489fb828a1d010
ManiSJ <ManiSJ@users.noreply.github.com>
2024-04-30 14:55:37 +1000

    Initial project

---
+++ src/main/java/com/example/GateEntryExit/GateEntryExitApplication.java
@@ -0,0 +1,13 @@
+package com.example.GateEntryExit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateEntryExitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateEntryExitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GateEntryExit/GateEntryExitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GateEntryExit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateEntryExitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
